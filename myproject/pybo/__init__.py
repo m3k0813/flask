e3318b223163f2dfff3cb17ec03d8f6368a23c42
m3k0813 <m3k0813@gmail.com>
2021-05-05 22:47:35 +0900

    2-06 answer function

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -1,5 +1,4 @@
 from flask import Flask
-# --------------------------------- [edit] ---------------------------------- #
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
@@ -7,8 +6,6 @@ import config
 
 db = SQLAlchemy()
 migrate = Migrate()
-# --------------------------------------------------------------------------- #
-
 
 def create_app():
     app = Flask(__name__)
@@ -18,8 +15,12 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
+    from . import models
+
     # 블루프린트
-    from .views import main_views
+    from .views import main_views, question_views, answer_views
     app.register_blueprint(main_views.bp)
+    app.register_blueprint(question_views.bp)
+    app.register_blueprint(answer_views.bp)
 
     return app
--- myproject/pybo/models.py
+++ myproject/pybo/models.py
@@ -1,14 +1,16 @@
 from pybo import db
 
+#질문 모델 id/제목/내용/작성일자
 class Question(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    subject = db.Column(db.String(200), nullable=False)
+    id = db.Column(db.Integer, primary_key=True)        #고유 번호
+    subject = db.Column(db.String(200), nullable=False)        #column 클래스는 데이터타입이나 속성을 지정
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
 
+#답변 모델
 class Answer(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))
-    question = db.relationship('Question', backref=db.backref('answer_set'))
+    question_id = db.Column(db.Integer, db.ForeignKey('question.id', ondelete='CASCADE'))      #질문 모델과 연결
+    question = db.relationship('Question', backref=db.backref('answer_set'))                   #relationship으로 속성 추가
     content = db.Column(db.Text(), nullable=False)
     create_date = db.Column(db.DateTime(), nullable=False)
