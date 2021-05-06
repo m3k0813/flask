e0087280e03cb7c6a162b5ba75ed4f6abfee08e3
m3k0813 <m3k0813@gmail.com>
2021-05-06 21:49:13 +0900

    2-09 complete

--- myproject/pybo/views/question_views.py
+++ myproject/pybo/views/question_views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from ..forms import QuestionForm
 
 from pybo.models import Question
 
@@ -15,3 +16,8 @@ def _list():
 def detail(question_id):
     question = Question.query.get_or_404(question_id)
     return render_template('question/question_detail.html', question=question)
+
+@bp.route('/create/')
+def create():
+    form = QuestionForm()
+    return render_template('question/question_form.html', form=form)
