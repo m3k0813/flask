370a4dc3408d42870480877b973f018986eaf90a
m3k0813 <m3k0813@gmail.com>
2021-05-08 20:58:47 +0900

    3-07 complete

--- myproject/pybo/__init__.py
+++ myproject/pybo/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
+
 import config
 
 db = SQLAlchemy()
@@ -18,9 +19,14 @@ def create_app():
     from . import models
 
     # 블루프린트
-    from .views import main_views, question_views, answer_views
+    from .views import main_views, question_views, answer_views, auth_views
     app.register_blueprint(main_views.bp)
     app.register_blueprint(question_views.bp)
     app.register_blueprint(answer_views.bp)
+    app.register_blueprint(auth_views.bp)
+
+    # 필터
+    from .filter import format_datetime
+    app.jinja_env.filters['datetime'] = format_datetime
 
     return app
